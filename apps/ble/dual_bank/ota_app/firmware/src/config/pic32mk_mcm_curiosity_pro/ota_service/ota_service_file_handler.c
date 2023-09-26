3e27c4ad6535d6165ade823b319fe35e5b577a27
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-09-26 18:32:12 +0530

    [APPS] Created ble_dual_bank_ota_app_pic32mk_mcm_curiosity_pro demo
    MH3-85948

---
+++ apps/ble/dual_bank/ota_app/firmware/src/config/pic32mk_mcm_curiosity_pro/ota_service/ota_service_file_handler.c
@@ -0,0 +1,345 @@
+/*******************************************************************************
+  OTA service File Handler Source File
+
+  File Name:
+    ota_service_file_handler.c
+
+  Summary:
+    This file contains source code necessary to handle the image and it's metadata.
+
+  Description:
+    This file contains source code necessary to handle the image and it's metadata.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2023 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Include Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "ota_service_file_handler.h"
+#include "ota_service.h"
+#include "definitions.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Type Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global objects
+// *****************************************************************************
+// *****************************************************************************
+
+static uint8_t CACHE_ALIGN controlBlockBuffer[OTA_CONTROL_BLOCK_BUFFER_SIZE];
+static uint8_t CACHE_ALIGN flash_data[DATA_SIZE];
+static OTA_FILE_HANDLER_CONTEXT otaFileHandlerCtx;
+static uint32_t ctrlBlkSize = OTA_CONTROL_BLOCK_BUFFER_SIZE;
+static OTA_FILE_HANDLER_DATA otaFileHandlerData =
+{
+    .state = OTA_SERVICE_FH_STATE_INIT,
+    .controlBlock = (OTA_CONTROL_BLOCK *)controlBlockBuffer
+};
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: File Handler Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* Function to Generate CRC by reading the firmware programmed into internal flash */
+static uint32_t OTA_SERVICE_FH_CRCGenerate(uint32_t start_addr, uint32_t size)
+{
+    uint32_t   i, j, value;
+    uint32_t   crc_tab[256];
+    uint32_t   crc = 0xFFFFFFFFU;
+    uint8_t    data;
+
+    for (i = 0U; i < 256U; i++)
+    {
+        value = i;
+
+        for (j = 0U; j < 8U; j++)
+        {
+            if ((value & 1U) != 0U)
+            {
+                value = (value >> 1U) ^ 0xEDB88320U;
+            }
+            else
+            {
+                value >>= 1U;
+            }
+        }
+        crc_tab[i] = value;
+    }
+
+    for (i = 0U; i < size; i++)
+    {
+        data = *(uint8_t *)KVA0_TO_KVA1((start_addr + i));
+
+        crc = crc_tab[(crc ^ data) & 0xffU] ^ (crc >> 8U);
+    }
+
+    return crc;
+}
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: File Handler Global Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* Trigger a reset */
+void OTA_SERVICE_FH_TriggerReset(void)
+{
+    /* Perform system unlock sequence */
+    SYSKEY = 0x00000000;
+    SYSKEY = 0xAA996655;
+    SYSKEY = 0x556699AA;
+
+    RSWRSTSET = _RSWRST_SWRST_MASK;
+    (void)RSWRST;
+}
+
+bool OTA_SERVICE_FH_CtrlBlkRead(OTA_CONTROL_BLOCK *controlBlock, uint32_t length)
+{
+    uint32_t appMetaDataAddress;
+    bool     status = false;
+    uint8_t  *ptrBuffer = (uint8_t *)controlBlock;
+
+    if ((ptrBuffer != NULL) && (length == ctrlBlkSize))
+    {
+        appMetaDataAddress  = (FLASH_END_ADDRESS - ctrlBlkSize);
+        (void)NVM_Read((void *)ptrBuffer, ctrlBlkSize, appMetaDataAddress);
+        status = true;
+    }
+
+    return status;
+}
+
+bool OTA_SERVICE_FH_CtrlBlkWrite(OTA_CONTROL_BLOCK *controlBlock, uint32_t length)
+{
+    uint32_t count;
+    uint32_t appMetaDataAddress;
+    bool     status = false;
+    uint8_t  *ptrBuffer = (uint8_t *)controlBlock;
+
+    if ((ptrBuffer != NULL) && (length == ctrlBlkSize))
+    {
+        appMetaDataAddress  = (FLASH_END_ADDRESS - ctrlBlkSize);
+
+
+        /* Erase the Current sector */
+        (void)NVM_PageErase(appMetaDataAddress);
+
+        while(NVM_IsBusy() == true)
+        {
+
+        }
+
+        for (count = 0; count < length; count += OTA_CONTROL_BLOCK_PAGE_SIZE)
+        {
+            (void)NVM_RowWrite((void *)ptrBuffer, appMetaDataAddress);
+
+            while(NVM_IsBusy() == true)
+            {
+
+            }
+            appMetaDataAddress += OTA_CONTROL_BLOCK_PAGE_SIZE;
+            ptrBuffer += OTA_CONTROL_BLOCK_PAGE_SIZE;
+        }
+
+        status = true;
+    }
+
+    return status;
+}
+
+OTA_SERVICE_FH_STATE OTA_SERVICE_FH_StateGet(void)
+{
+    return otaFileHandlerData.state;
+}
+
+void OTA_SERVICE_FH_Tasks(void)
+{
+    /* Check the application's current state. */
+    switch (otaFileHandlerData.state)
+    {
+        case OTA_SERVICE_FH_STATE_INIT:
+        {
+            otaFileHandlerData.nFlashBytesWritten = 0U;
+            otaFileHandlerData.nFlashBytesFreeSpace = DATA_SIZE;
+            otaFileHandlerData.totalBytesWritten = 0U;
+            otaFileHandlerData.controlBlock->ActiveImageNum = 0U;
+            otaFileHandlerCtx.fileHeader = &otaFileHandlerData.fileHeader;
+            otaFileHandlerData.state = OTA_SERVICE_FH_STATE_MSG_RECV;
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_MSG_RECV:
+        {
+            uint32_t receivedBytes;
+
+            otaFileHandlerCtx.buffer = &flash_data[otaFileHandlerData.nFlashBytesWritten];
+            otaFileHandlerCtx.size = otaFileHandlerData.nFlashBytesFreeSpace;
+            receivedBytes = OTA_SERVICE_Transport_FHMsgReceive(&otaFileHandlerCtx);
+
+            if (receivedBytes != 0U)
+            {
+                otaFileHandlerData.nFlashBytesWritten += receivedBytes;
+                otaFileHandlerData.nFlashBytesFreeSpace -= receivedBytes;
+
+                if ((otaFileHandlerData.nFlashBytesWritten >= DATA_SIZE)
+                ||  ((otaFileHandlerData.initData == true) && ((otaFileHandlerData.nFlashBytesWritten + otaFileHandlerData.totalBytesWritten) >= otaFileHandlerData.fileHeader.imageSize)))
+                {
+                    if (otaFileHandlerData.initData == false)
+                    {
+                        otaFileHandlerData.initData = true;
+                        otaFileHandlerData.memoryAddress = otaFileHandlerData.fileHeader.programAddress;
+                    }
+
+                    if (0U == (otaFileHandlerData.memoryAddress % ERASE_BLOCK_SIZE))
+                    {
+                        otaFileHandlerData.state = OTA_SERVICE_FH_STATE_ERASE;
+                    }
+                    else
+                    {
+                        otaFileHandlerData.state = OTA_SERVICE_FH_STATE_WRITE;
+                    }
+                }
+            }
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_ERASE:
+        {
+
+            /* Erase the Current sector */
+            (void)NVM_PageErase(otaFileHandlerData.memoryAddress);
+
+            otaFileHandlerData.state = OTA_SERVICE_FH_STATE_ERASE_WAIT;
+
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_WRITE:
+        {
+            (void)NVM_RowWrite((void *)&flash_data[0], otaFileHandlerData.memoryAddress);
+            otaFileHandlerData.nFlashBytesWritten = 0U;
+            otaFileHandlerData.nFlashBytesFreeSpace = DATA_SIZE;
+            otaFileHandlerData.memoryAddress += DATA_SIZE;
+            otaFileHandlerData.totalBytesWritten += DATA_SIZE;
+            otaFileHandlerData.state = OTA_SERVICE_FH_STATE_WRITE_WAIT;
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_ERASE_WAIT:
+        case OTA_SERVICE_FH_STATE_WRITE_WAIT:
+        {
+            if (NVM_IsBusy() == false)
+            {
+                if (otaFileHandlerData.state == OTA_SERVICE_FH_STATE_ERASE_WAIT)
+                {
+                    otaFileHandlerData.state = OTA_SERVICE_FH_STATE_WRITE;
+                }
+                else
+                {
+                    if (otaFileHandlerData.totalBytesWritten >= otaFileHandlerData.fileHeader.imageSize)
+                    {
+                        otaFileHandlerData.state = OTA_SERVICE_FH_STATE_VERIFY;
+                    }
+                    else
+                    {
+                        otaFileHandlerData.state = OTA_SERVICE_FH_STATE_MSG_RECV;
+                    }
+                }
+            }
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_VERIFY:
+        {
+            uint32_t crc32;
+            uint32_t *received_crc32 = (void *)otaFileHandlerData.fileHeader.signature;
+
+            crc32 = OTA_SERVICE_FH_CRCGenerate(otaFileHandlerData.fileHeader.programAddress, otaFileHandlerData.fileHeader.imageSize);
+
+            if (crc32 == *received_crc32)
+            {
+                otaFileHandlerData.state = OTA_SERVICE_FH_STATE_CB_WRITE;
+            }
+            else
+            {
+                otaFileHandlerData.state = OTA_SERVICE_FH_STATE_IDLE;
+            }
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_CB_WRITE:
+        {
+            otaFileHandlerData.controlBlock->ActiveImageNum = 0U;
+            otaFileHandlerData.controlBlock->versionNum = 0U;
+            otaFileHandlerData.controlBlock->blockUpdated = 1U;
+            otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].imageType = otaFileHandlerData.fileHeader.imageType;
+            otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].status = otaFileHandlerData.fileHeader.status;
+            otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].programAddress = otaFileHandlerData.fileHeader.programAddress;
+            otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].jumpAddress = otaFileHandlerData.fileHeader.jumpAddress;
+            otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].loadAddress = otaFileHandlerData.fileHeader.loadAddress;
+            otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].imageSize = otaFileHandlerData.fileHeader.imageSize;
+            otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].signaturePresent = 1U;
+            (void)memcpy(otaFileHandlerData.controlBlock->appImageInfo[otaFileHandlerData.controlBlock->ActiveImageNum].signature, otaFileHandlerData.fileHeader.signature, sizeof(otaFileHandlerData.fileHeader.signature));
+
+            if (OTA_SERVICE_FH_CtrlBlkWrite(otaFileHandlerData.controlBlock, ctrlBlkSize) == false)
+            {
+                otaFileHandlerData.state = OTA_SERVICE_FH_STATE_ERROR;
+            }
+            else
+            {
+                otaFileHandlerData.state = OTA_SERVICE_FH_STATE_IDLE;
+            }
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_IDLE:
+        {
+            break;
+        }
+
+        case OTA_SERVICE_FH_STATE_ERROR:
+        default:
+        {
+            // Do nothing
+            break;
+        }
+    }
+}
---
+++ apps/ble/dual_bank/ota_app/firmware/src/config/pic32mk_mcm_curiosity_pro/ota_service/ota_service_file_handler.h
@@ -0,0 +1,163 @@
+/*******************************************************************************
+  OTA service File Handler Header File
+
+  File Name:
+    ota_service_file_handler.h
+
+  Summary:
+    This file contains OTA service File Handler definitions and functions.
+
+  Description:
+    This file contains OTA service File Handler definitions and functions.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2023 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Include Files
+// *****************************************************************************
+// *****************************************************************************
+
+#ifndef OTA_SERVICE_FILE_HANDLER_H
+#define OTA_SERVICE_FILE_HANDLER_H
+
+#include "ota_service_control_block.h"
+#include "configuration.h"
+
+/* Provide C++ Compatibility */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Type Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+#define FLASH_START                      (0x9d000000UL)
+#define FLASH_LENGTH                     (0x100000UL)
+#define PAGE_SIZE                        (512UL)
+#define ERASE_BLOCK_SIZE                 (4096UL)
+
+/* Starting location of Bootloader in Inactive bank */
+#define INACTIVE_BANK_OFFSET             (FLASH_LENGTH / 2U)
+#define INACTIVE_BANK_START              (FLASH_START + INACTIVE_BANK_OFFSET)
+#define FLASH_END_ADDRESS                (INACTIVE_BANK_START + INACTIVE_BANK_OFFSET)
+
+
+#define DATA_SIZE                        PAGE_SIZE
+
+#define BUFFER_SIZE(x, y)                ((y) > (x)? ((((y) % (x)) != 0U)?((((y) / (x)) + 1U) * (x)) : (((y) / (x)) * (x))) : (x))
+
+#define OTA_CONTROL_BLOCK_SIZE           sizeof(OTA_CONTROL_BLOCK)
+#define OTA_CONTROL_BLOCK_PAGE_SIZE      PAGE_SIZE
+
+#define OTA_CONTROL_BLOCK_BUFFER_SIZE    BUFFER_SIZE(OTA_CONTROL_BLOCK_PAGE_SIZE, OTA_CONTROL_BLOCK_SIZE)
+
+
+typedef enum
+{
+    OTA_SERVICE_FH_STATE_INIT = 0,
+
+    OTA_SERVICE_FH_STATE_MSG_RECV,
+
+    OTA_SERVICE_FH_STATE_ERASE,
+
+    OTA_SERVICE_FH_STATE_ERASE_WAIT,
+
+    OTA_SERVICE_FH_STATE_WRITE,
+
+    OTA_SERVICE_FH_STATE_WRITE_WAIT,
+
+    OTA_SERVICE_FH_STATE_VERIFY,
+
+    OTA_SERVICE_FH_STATE_CB_WRITE,
+
+    OTA_SERVICE_FH_STATE_IDLE,
+
+    OTA_SERVICE_FH_STATE_ERROR
+
+} OTA_SERVICE_FH_STATE;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t  imageIdentity[11];
+    uint8_t  headerVersion;
+    uint8_t  signaturePresent;
+    uint8_t  status;
+    uint8_t  imageStorage;
+    uint8_t  imageType;
+    uint32_t programAddress;
+    uint32_t jumpAddress;
+    uint32_t imageSize;
+    uint32_t loadAddress;
+    uint8_t  versionNumber;
+    uint8_t  signature[8];
+    uint8_t  signatureType;
+} OTA_FILE_HEADER;
+
+typedef struct
+{
+    OTA_SERVICE_FH_STATE state;
+
+    uint32_t nFlashBytesWritten;
+
+    uint32_t totalBytesWritten;
+
+    uint32_t nFlashBytesFreeSpace;
+
+    uint32_t memoryAddress;
+
+    bool     initData;
+
+    OTA_FILE_HEADER fileHeader;
+
+    OTA_CONTROL_BLOCK *controlBlock;
+} OTA_FILE_HANDLER_DATA;
+
+typedef struct
+{
+    void *buffer;
+
+    uint32_t size;
+
+    OTA_FILE_HEADER *fileHeader;
+} OTA_FILE_HANDLER_CONTEXT;
+
+void OTA_SERVICE_FH_TriggerReset(void);
+bool OTA_SERVICE_FH_CtrlBlkRead(OTA_CONTROL_BLOCK *controlBlock, uint32_t length);
+bool OTA_SERVICE_FH_CtrlBlkWrite(OTA_CONTROL_BLOCK *controlBlock, uint32_t length);
+OTA_SERVICE_FH_STATE OTA_SERVICE_FH_StateGet(void);
+void OTA_SERVICE_FH_Tasks(void);
+
+/* Provide C++ Compatibility */
+#ifdef __cplusplus
+}
+#endif
+
+#endif      //OTA_SERVICE_FILE_HANDLER_H
